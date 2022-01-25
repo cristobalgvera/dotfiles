a2fcc1542e807c9130a875f05072ada8edc6a9e2
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-01-25 02:06:53 -0300

    Formatted bash files

--- scripts/macos/add-symlinks.sh
+++ scripts/macos/add-symlinks.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 function link_file {
-    local sourceFile=$1
-    local targetFile=$2
+	local sourceFile=$1
+	local targetFile=$2
 
-    echo "Linking: ${sourceFile} to ${targetFile}"
+	echo "Linking: ${sourceFile} to ${targetFile}"
 
-    if [ -e $targetFile ]; then
-        echo "Creating backup of $targetFile"
-        mv $targetFile $targetFile.backup
-    fi
+	if [ -e "$targetFile" ]; then
+		echo "Creating backup of $targetFile"
+		mv "$targetFile" "$targetFile".backup
+	fi
 
-    ln -sf $sourceFile $targetFile
+	ln -sf "$sourceFile" "$targetFile"
 }
 
 function add_symlinks {
-    local basePath=$HOME/Documents/Repositories/dotfiles/$(dirname $0)/../..
-    local sharedPath=$basePath/shared
-    local macosFilesPath=$basePath/macos/files
+	local basePath=$HOME/Documents/Repositories/dotfiles/$(dirname $0)/../..
+	local sharedPath=$basePath/shared
+	local macosFilesPath=$basePath/macos/files
 
-    # Git config
-    echo "Linking Git config"
-    link_file $sharedPath/.gitconfig ~/.gitconfig
-    link_file $macosFilesPath/.gitconfig-macos ~/.gitconfig-macos
+	# Git config
+	echo "Linking Git config"
+	link_file "$sharedPath"/.gitconfig ~/.gitconfig
+	link_file "$macosFilesPath"/.gitconfig-macos ~/.gitconfig-macos
 
-    # Ideavim config
-    echo "Linking Ideavim config"
-    link_file $sharedPath/.ideavimrc ~/.ideavimrc
+	# Ideavim config
+	echo "Linking Ideavim config"
+	link_file "$sharedPath"/.ideavimrc ~/.ideavimrc
 
-    # Zsh config
-    echo "Linking Zsh config"
-    link_file $macosFilesPath/.zshrc ~/.zshrc
+	# Zsh config
+	echo "Linking Zsh config"
+	link_file "$macosFilesPath"/.zshrc ~/.zshrc
 
-    # LunarVim config
-    echo "Linking LunarVim config"
-    link_file $macosFilesPath/lvim-init.lua ~/.config/lvim/config.lua
+	# LunarVim config
+	echo "Linking LunarVim config"
+	link_file "$macosFilesPath"/lvim-init.lua ~/.config/lvim/config.lua
 }
 
 add_symlinks
--- scripts/macos/install-apps.sh
+++ scripts/macos/install-apps.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 function install_apps {
-    local basePath=$(dirname $0)
+	local basePath=$(dirname $0)
 
-    echo "Installing apps..."
+	echo "Installing apps..."
 
-    # Homebrew
-    if ! [ -x "$(command -v brew)" ]; then
-        echo "Installing Homebrew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
-    fi
+	# Homebrew
+	if ! [ -x "$(command -v brew)" ]; then
+		echo "Installing Homebrew..."
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+	fi
 
-    brew update
+	brew update
 
-    # Formulae
-    local formulaes=(
-        go
-        fzf
-        git
-        neovim
-    )
+	# Formulae
+	local formulaes=(
+		go
+		fzf
+		git
+		neovim
+		jesseduffield/lazygit/lazygit
+	)
 
-    for formulae in "${formulaes[@]}"; do
-        /bin/bash -c "($basePath/brew-install.sh $formulae)"
-    done
+	for formulae in "${formulaes[@]}"; do
+		/bin/bash -c "($basePath/brew-install.sh $formulae)"
+	done
 
-    # Create a Go workspace if it doesn't exist
-    if [ ! -d $HOME/go ]; then
-        mkdir -p $HOME/go/{src,pkg,bin}
-    fi
+	# Create a Go workspace if it doesn't exist
+	if [ ! -d "$HOME"/go ]; then
+		mkdir -p "$HOME"/go/{src,pkg,bin}
+	fi
 
-    # Cask
-    local casks=(
-        fig
-        font-caskaydia-cove-nerd-font
-        font-source-code-pro
-        google-chrome
-        google-drive
-        iterm2
-        rar
-        raycast
-        visual-studio-code
-        whatsapp
-    )
+	# Cask
+	local casks=(
+		fig
+		font-caskaydia-cove-nerd-font
+		font-source-code-pro
+		google-chrome
+		google-drive
+		iterm2
+		rar
+		raycast
+		visual-studio-code
+		whatsapp
+	)
 
-    for cask in "${casks[@]}"; do
-        /bin/bash -c "($basePath/brew-install.sh $cask cask)"
-    done
+	for cask in "${casks[@]}"; do
+		/bin/bash -c "($basePath/brew-install.sh $cask cask)"
+	done
 }
 
 install_apps
