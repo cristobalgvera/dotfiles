1ec8b79ce2c32b64e91dd0c08c232f7f8d1019c5
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-01-25 02:02:00 -0300

    Added Refactoring support

--- shared/folders/nvim/lua/user/plugins.lua
+++ shared/folders/nvim/lua/user/plugins.lua
@@ -75,6 +75,7 @@ return packer.startup({function(use)
     "williamboman/nvim-lsp-installer",          -- simple to use language server installer
     "tamago324/nlsp-settings.nvim",             -- language server settings defined in json for
     "ray-x/lsp_signature.nvim",                 -- show method's signature
+    "ThePrimeagen/refactoring.nvim",            -- code refactoring base in good practices
 
     -- Null-ls
     "jose-elias-alvarez/null-ls.nvim",           -- for formatters and linters
