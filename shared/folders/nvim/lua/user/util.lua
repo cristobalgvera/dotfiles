eb1ea69f6d687520711a60aec0442ca74044a31b
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-01-31 19:01:01 -0300

    Added Set data type utility function

--- shared/folders/nvim/lua/user/util.lua
+++ shared/folders/nvim/lua/user/util.lua
@@ -2,4 +2,14 @@ local M = {}
 
 M.is_windows = vim.fn.has("win32") == 1
 
+M.Set = function (items)
+  local set = {}
+
+  for _, item in ipairs(items) do
+    set[item] = true
+  end
+
+  return set
+end
+
 return M
