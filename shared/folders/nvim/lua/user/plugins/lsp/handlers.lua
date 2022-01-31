7454c6df05801cf4ae68d3c433820da4b7dc8fa5
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-01-31 19:04:27 -0300

    Improved custom capabilities execution for LSP handlers

--- shared/folders/nvim/lua/user/plugins/lsp/handlers.lua
+++ shared/folders/nvim/lua/user/plugins/lsp/handlers.lua
@@ -1,148 +1,161 @@
+local util = require("user.util")
 local M = {}
 
 local max_width = math.max(math.floor(vim.o.columns * 0.7), 100)
 local max_height = math.max(math.floor(vim.o.lines * 0.3), 30)
 
 M.setup = function()
-  local signs = {
-    Error = "",
-    Warn = "",
-    Hint = "",
-    Info = "",
-  }
+	local signs = {
+		Error = "",
+		Warn = "",
+		Hint = "",
+		Info = "",
+	}
 
-  for type, icon in pairs(signs) do
-    local name = "DiagnosticSign" .. type
-    vim.fn.sign_define(name, {texthl = name, text = icon, numhl = ""})
-  end
+	for type, icon in pairs(signs) do
+		local name = "DiagnosticSign" .. type
+		vim.fn.sign_define(name, { texthl = name, text = icon, numhl = "" })
+	end
 
-  vim.diagnostic.config {
-    virtual_text = true,
-    signs = { active = signs }, -- show signs
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = false,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  }
+	vim.diagnostic.config({
+		virtual_text = true,
+		signs = { active = signs }, -- show signs
+		update_in_insert = true,
+		underline = true,
+		severity_sort = true,
+		float = {
+			focusable = false,
+			style = "minimal",
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
+	})
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-    max_width = max_width,
-    max_height = max_height,
-  })
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+		border = "rounded",
+		max_width = max_width,
+		max_height = max_height,
+	})
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-    max_width = max_width,
-    max_height = max_height,
-  })
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+		border = "rounded",
+		max_width = max_width,
+		max_height = max_height,
+	})
 end
 
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-    [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
+        augroup lsp_document_highlight
+          autocmd! * <buffer>
+          autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+      ]],
+			false
+		)
+	end
 end
 
 local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  local keymap = vim.api.nvim_buf_set_keymap
+	local opts = { noremap = true, silent = true }
+	local keymap = vim.api.nvim_buf_set_keymap
 
-  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap(bufnr, "n", "<Leader>cs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  keymap(bufnr, "n", "<Leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  keymap(bufnr, "n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  keymap(bufnr, "n", "<Leader>cd", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  keymap(bufnr, "n", "<Leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  -- vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	keymap(bufnr, "n", "<Leader>cs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	keymap(bufnr, "n", "<Leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	keymap(bufnr, "n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+	keymap(bufnr, "n", "<Leader>cd", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	keymap(bufnr, "n", "<Leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+end
+
+local function define_capabilities(client, opts)
+	local capabilities = client.resolved_capabilities
+
+	if not opts then
+		return capabilities
+	end
+
+	if opts.disable_formatting then
+		capabilities.document_formatting = false
+		capabilities.document_range_formatting = false
+	end
+
+	return capabilities
 end
 
 local function define_client_capabilities(client)
-  local base_opts = {
-    disable_formatting = true
-  }
+	local base_opts = {
+		disable_formatting = true,
+	}
 
-  local function define_capabilities(opts)
-    local capabilities = client.resolved_capabilities
+	local custom_capabilities_langs = util.Set({
+		"tsserver",
+		"rust_analyzer",
+		"html",
+	})
 
-    if not opts then
-      return capabilities
-    end
+	-- local snippet_support_langs = util.Set({
+	-- 	"jsonls",
+	-- 	"cssls",
+	-- 	"html",
+	-- 	"emmet-ls",
+	-- })
 
-    if opts.disable_formatting then
-      capabilities.document_formatting = false
-      capabilities.document_range_formatting = false
-    end
+	if custom_capabilities_langs[client.name] then
+		define_capabilities(client, base_opts)
+	end
 
-    return capabilities
-  end
-
-  if client.name == "tsserver" then
-    define_capabilities(base_opts)
-  end
-
-  if client.name == "rust_analyzer" then
-    define_capabilities(base_opts)
-  end
-
-  if client.name == "jsonls" then
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-  end
+	-- if snippet_support_langs[client.name] then
+	-- 	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	-- 	capabilities.textDocument.completion.completionItem.snippetSupport = true
+	-- end
 end
 
 local function make_signature_help(bufnr)
-  local status_ok, lsp_signature = pcall(require, "lsp_signature")
-  if status_ok then
-    lsp_signature.on_attach({
-      bind = true,                -- This is mandatory, otherwise border config won't get registered.
-      handler_opts = {
-        border = "rounded"
-      },
-      hint_prefix = "📝 ",        -- virtual text prefix icon
-      floating_window = false,    -- show hint in a floating window, set to false for virtual text only mode
-      transparency = nil,         -- disabled by default, allow floating win transparent value 1~100
-      toggle_key = "<C-s>",       -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-      max_height = max_height,    -- max height of signature floating_window, if content is more than max_height, can scroll down
-      max_width = max_width,      -- max_width of signature floating_window, line will be wrapped if exceed max_width
-    }, bufnr)
-  end
+	local status_ok, lsp_signature = pcall(require, "lsp_signature")
+	if not status_ok then
+		return
+	end
+
+	lsp_signature.on_attach({
+		bind = true, -- This is mandatory, otherwise border config won't get registered.
+		handler_opts = {
+			border = "rounded",
+		},
+		hint_prefix = "📝 ", -- virtual text prefix icon
+		floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+		transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+		toggle_key = "<C-s>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+		max_height = max_height, -- max height of signature floating_window, if content is more than max_height, can scroll down
+		max_width = max_width, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+	}, bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  define_client_capabilities(client)
-  lsp_highlight_document(client)
-  lsp_keymaps(bufnr)
-  make_signature_help(bufnr)
+	define_client_capabilities(client)
+	lsp_highlight_document(client)
+	lsp_keymaps(bufnr)
+	make_signature_help(bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-  return
+	return
 end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
