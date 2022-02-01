a73a6896d56759fce088ac27a6db756dc19c0828
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-01-31 22:27:32 -0300

    Added indentation to NvimTree and update usage of some configurations

--- shared/folders/nvim/lua/user/plugins/nvim-tree.lua
+++ shared/folders/nvim/lua/user/plugins/nvim-tree.lua
@@ -70,15 +70,22 @@ nvim_tree.setup({
 		cmd = "trash",
 		require_confirm = true,
 	},
+})
+
+local options = {
+	indent_markers = 1,
 	quit_on_open = 1,
 	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
 	show_icons = {
 		git = 1,
 		folders = 1,
 		files = 1,
-		folder_arrows = 1,
-		tree_width = 30,
+		folder_arrows = 0,
 	},
-})
+  -- add_trailing = 1,
+  -- group_empty = 1,
+}
+
+for option, value in pairs(options) do
+	vim.g["nvim_tree_" .. option] = value
+end
