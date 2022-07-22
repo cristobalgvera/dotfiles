f4a0dc955a19e84f8309313115baebe0975d57a2
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-07-22 15:34:39 -0400

    feat(lsp): removed aws support for yaml files

--- shared/folders/nvim/lua/user/plugins/lsp/settings/yamlls.lua
+++ shared/folders/nvim/lua/user/plugins/lsp/settings/yamlls.lua
@@ -1,25 +1,6 @@
-local schemas = {
-  ["https://s3.amazonaws.com/cfn-resource-specifications-us-east-1-prod/schemas/2.15.0/all-spec.json"] = "*.yml",
-}
+local schemas = {}
 
-local custom_tags = {
-  "!And",
-  "!If",
-  "!Not",
-  "!Equals",
-  "!Or",
-  "!FindInMap",
-  "!Base64",
-  "!Cidr",
-  "!Ref",
-  "!Sub",
-  "!GetAtt",
-  "!GetAZs",
-  "!ImportValue",
-  "!Select",
-  "!Split",
-  "!Join",
-}
+local custom_tags = {}
 
 local opts = {
   settings = {
