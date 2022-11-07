7359122ff246cdb18c1a03a530d939ae79732fb7
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-07 09:30:38 -0300

    feat(lvim): add null-ls linters

--- shared/folders/lvim/lua/user/core/lsp/null-ls/init.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/init.lua
@@ -1,5 +1,6 @@
 local null_ls_builtins = {
   "formatters",
+  "linters",
 }
 
 for _, builtin in ipairs(null_ls_builtins) do
---
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/linters.lua
@@ -0,0 +1,5 @@
+local linters = require_safe("lvim.lsp.null-ls.linters")
+
+linters.setup({
+  { command = "markdownlint" }, -- yarn global add markdownlint
+})
