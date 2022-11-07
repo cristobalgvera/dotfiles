c3bdc8ac83b053bf1336b58bf03643562d364d8d
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-07 09:25:00 -0300

    feat(lvim): add prettier and rustfmt null-ls formatters

--- shared/folders/lvim/lua/user/core/lsp/null-ls/formatters.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/formatters.lua
@@ -1,5 +1,28 @@
 local formatters = require_safe("lvim.lsp.null-ls.formatters")
 
 formatters.setup({
-  { command = "stylua", filetypes = { "lua" } }, -- cargo install stylua
+  { command = "stylua" }, -- cargo install stylua
+  {
+    command = "prettier", -- yarn global add prettier
+    extra_filetypes = { "toml" },
+    timeout = 1000,
+  },
+  {
+    command = "rustfmt", -- rustup component add rustfmt
+    extra_args = function(params)
+      local Path = require("plenary.path")
+      local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+      if cargo_toml:exists() and cargo_toml:is_file() then
+        for _, line in ipairs(cargo_toml:readlines()) do
+          local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+          if edition then
+            return { "--edition=" .. edition }
+          end
+        end
+      end
+      -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+      return { "--edition=2021" }
+    end,
+  },
 })
