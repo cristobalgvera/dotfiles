12e5b6bc00845ed6829d00a0970cf3f83c7ae397
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-11-09 21:02:53 -0300

    style(macos): unify format to .sh files

--- scripts/macos/add-symlinks.sh
+++ scripts/macos/add-symlinks.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 function link_file {
-	local sourceFile=$1
-	local targetFile=$2
+  local sourceFile=$1
+  local targetFile=$2
 
-	echo "Linking: ${sourceFile} to ${targetFile}"
+  echo "Linking: ${sourceFile} to ${targetFile}"
 
-	if [ -e "$targetFile" ]; then
-		echo "Creating backup of $targetFile"
-		mv "$targetFile" "$targetFile".backup
-	fi
+  if [ -e "$targetFile" ]; then
+    echo "Creating backup of $targetFile"
+    mv "$targetFile" "$targetFile".backup
+  fi
 
-	ln -sf "$sourceFile" "$targetFile"
+  ln -sf "$sourceFile" "$targetFile"
 }
 
 function add_symlinks {
-	local basePath=$HOME/Documents/Repositories/dotfiles/$(dirname "$0")/../..
-	local sharedPath=$basePath/shared
-	local macosFilesPath=$basePath/macos/files
+  local basePath=$HOME/Documents/Repositories/dotfiles/$(dirname "$0")/../..
+  local sharedPath=$basePath/shared
+  local macosFilesPath=$basePath/macos/files
   local macosFoldersPath=$basePath/macos/folders
 
-	# Git config
-	echo "Linking Git config"
-	link_file "$sharedPath"/.gitconfig ~/.gitconfig
-	link_file "$macosFilesPath"/.gitconfig-macos ~/.gitconfig-macos
+  # Git config
+  echo "Linking Git config"
+  link_file "$sharedPath"/.gitconfig ~/.gitconfig
+  link_file "$macosFilesPath"/.gitconfig-macos ~/.gitconfig-macos
 
-	# Ideavim config
-	echo "Linking Ideavim config"
-	link_file "$sharedPath"/.ideavimrc ~/.ideavimrc
+  # Ideavim config
+  echo "Linking Ideavim config"
+  link_file "$sharedPath"/.ideavimrc ~/.ideavimrc
 
-	# LunarVim config
-	echo "Linking LunarVim config"
-	link_file "$sharedPath"/folders/lvim ~/.config/lvim
+  # LunarVim config
+  echo "Linking LunarVim config"
+  link_file "$sharedPath"/folders/lvim ~/.config/lvim
 
   # Warp config
   echo "Linking Warp config"
   link_file "$macosFoldersPath"/.warp ~/.warp
 
-	# Lazygit config
-	echo "Linking Lazygit config"
-	link_file "$sharedPath"/config.lazygit.yml ~/Library/Application\ Support/lazygit/config.yml
+  # Lazygit config
+  echo "Linking Lazygit config"
+  link_file "$sharedPath"/config.lazygit.yml ~/Library/Application\ Support/lazygit/config.yml
 
-	# Zsh config
-	echo "Linking Zsh config"
-	link_file "$macosFilesPath"/.zshrc ~/.zshrc
+  # Zsh config
+  echo "Linking Zsh config"
+  link_file "$macosFilesPath"/.zshrc ~/.zshrc
 
-	# Starship config
-	echo "Linking Starship config"
+  # Starship config
+  echo "Linking Starship config"
   link_file "$sharedPath"/starship.toml ~/.config/starship.toml
 }
 
--- scripts/macos/brew-install.sh
+++ scripts/macos/brew-install.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 function install_cask {
-	if brew list --cask --versions "$1" &>/dev/null; then
-		echo "$1 is already installed"
-	else
-		brew install --cask "$1"
-		echo "cask '$1' was installed"
-	fi
+  if brew list --cask --versions "$1" &>/dev/null; then
+    echo "$1 is already installed"
+  else
+    brew install --cask "$1"
+    echo "cask '$1' was installed"
+  fi
 }
 
 function install_tap {
-  	brew tap "$1"
-  	echo "tap '$1' was installed"
+  brew tap "$1"
+  echo "tap '$1' was installed"
 }
 
 function install_formulae {
-	if brew list --versions "$1" &>/dev/null; then
-		echo "$1 is already installed"
-	else
-		brew install "$1"
-		echo "formulae '$1' was installed"
-	fi
+  if brew list --versions "$1" &>/dev/null; then
+    echo "$1 is already installed"
+  else
+    brew install "$1"
+    echo "formulae '$1' was installed"
+  fi
 }
 
 function brew_install {
-	echo "Installing '$1'..."
+  echo "Installing '$1'..."
 
-  	case "$2" in
-  	  	cask)
-  	    		install_cask "$1"
-  	    		;;
+  case "$2" in
+    cask)
+      install_cask "$1"
+      ;;
 
-  	  	tap)
-  	    		install_tap "$1"
-  	    		;;
+    tap)
+      install_tap "$1"
+      ;;
 
-  	  	*)
-  	    		install_formulae "$1"
-  	    		;;
-  	esac
+    *)
+      install_formulae "$1"
+      ;;
+  esac
 }
 
 brew_install "$1" "$2"
