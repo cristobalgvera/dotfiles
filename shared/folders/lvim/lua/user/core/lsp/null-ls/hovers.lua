7ac35095fb284e32be6a07c20cb7e76c41c46887
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-12-13 16:27:00 -0300

    refactor(lvim): rename hover to hovers instead

--- shared/folders/lvim/lua/user/core/lsp/null-ls/hovers.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/hovers.lua
@@ -1,5 +1,5 @@
 -- TODO: Include hovers from LunarVim when it's available
-local hovers = reload("user.util.null-ls.hover")
+local hovers = reload("user.util.null-ls.hovers")
 
 hovers.setup({
   { command = "printenv" },
