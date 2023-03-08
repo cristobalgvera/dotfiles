460f64d20daf109803753e62368faf378b9d9eba
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-08 15:46:58 -0300

    refactor(lvim): replace keymap to handle themes

--- shared/folders/lvim/lua/user/themes/keymaps.lua
+++ shared/folders/lvim/lua/user/themes/keymaps.lua
@@ -1,6 +1,6 @@
 local which_key = lvim.builtin.which_key
 
-which_key.mappings[","] = {
+which_key.mappings["\\"] = {
   name = "+Themes",
   t = { "<cmd>lua require('user.themes').toggle_theme()<cr>", "Toggle light/dark theme" },
 }
