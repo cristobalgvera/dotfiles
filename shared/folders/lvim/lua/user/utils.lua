ca7eb9284188323c192f792c9a7a37e52a01e03d
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-08 13:15:04 -0300

    feat(lvim): create setup_from utility function to iterate over config files

--- shared/folders/lvim/lua/user/utils.lua
+++ shared/folders/lvim/lua/user/utils.lua
@@ -11,8 +11,32 @@ M.require_from = function(base_mod)
   --- Iterate over a table of files names and require them
   --- @param file_names table<any, string>
   return function(file_names)
+    local files = {}
+
     for _, file_name in ipairs(file_names) do
-      require(base_mod .. "." .. file_name)
+      files[file_name] = require(base_mod .. "." .. file_name)
+    end
+
+    return files
+  end
+end
+
+--- Obtain an iterator function to setup
+--- configuration files from a given base path
+--- @param base_mod string
+M.setup_from = function(base_mod)
+  --- Iterate over a table of configuration
+  --- files and setup them
+  --- @param config_file_names table<any, string> Each file needs to have a `setup` function declared
+  return function(config_file_names)
+    local config = M.require_from(base_mod)(config_file_names)
+
+    for file_name, file in pairs(config) do
+      if file.setup then
+        file.setup()
+      else
+        vim.notify(string.format("No setup function found in %s", file_name), "error")
+      end
     end
   end
 end
