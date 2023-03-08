9702c521ca9dfa4e7cf7a55390135c383fc4515c
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-08 16:08:51 -0300

    feat(lvim): include eslint as null-ls linter and code-action provider

--- shared/folders/lvim/lua/user/core/lsp/null-ls/code-actions.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/code-actions.lua
@@ -2,4 +2,13 @@ local code_actions = reload("lvim.lsp.null-ls.code_actions")
 
 code_actions.setup({
   { command = "shellcheck" }, -- brew install shellcheck
+  {
+    command = "eslint",
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+  }, -- npm install eslint
 })
--- shared/folders/lvim/lua/user/core/lsp/null-ls/linters.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/linters.lua
@@ -3,4 +3,13 @@ local linters = reload("lvim.lsp.null-ls.linters")
 linters.setup({
   { command = "markdownlint" }, -- yarn global add markdownlint
   { command = "shellcheck" }, -- brew install shellcheck
+  {
+    command = "eslint",
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+  }, -- npm install eslint
 })
