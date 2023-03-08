2a6fee1743cdf92bee25dbe0ea81c0cdb0bca117
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-08 14:03:28 -0300

    refactor(lvim): prioritize usage of vim.api to create autocmds instead of lvim utility

--- shared/folders/lvim/lua/user/autocommands.lua
+++ shared/folders/lvim/lua/user/autocommands.lua
@@ -1,12 +1,7 @@
-lvim.autocommands = {
-  {
-    "BufEnter",
-    {
-      desc = "Disable diagnostics on environment files",
-      pattern = { ".env*" },
-      callback = function(args)
-        vim.diagnostic.disable(args.buf)
-      end,
-    },
-  },
-}
+vim.api.nvim_create_autocmd("BufEnter", {
+  desc = "Disable diagnostics on environment files",
+  pattern = { ".env*" },
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end,
+})
