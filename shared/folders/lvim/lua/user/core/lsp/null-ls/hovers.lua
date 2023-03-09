b215ad21797a5b65f2981ae58aaf65ec35992e81
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-09 10:42:02 -0300

    feat(lvim): remove hover implementation to null-ls

--- shared/folders/lvim/lua/user/core/lsp/null-ls/hovers.lua
+++
@@ -1,6 +0,0 @@
--- TODO: Include hovers from LunarVim when it's available
-local hovers = reload("user.util.null-ls.hovers")
-
-hovers.setup({
-  { command = "printenv" },
-})
--- shared/folders/lvim/lua/user/core/lsp/null-ls/init.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/init.lua
@@ -2,7 +2,6 @@ local utils = require("user.utils")
 
 utils.require_from("user.core.lsp.null-ls")({
   "formatters",
-  "hovers",
   "linters",
   "code-actions",
 })
