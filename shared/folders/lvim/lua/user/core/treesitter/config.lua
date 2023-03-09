9d64615ae3bd03415d305f63d1f3d0afd3b448cb
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-09 13:49:37 -0300

    refactor(lvim): move treesitter to module-like directory

---
+++ shared/folders/lvim/lua/user/core/treesitter/config.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.setup = function()
+  local treesitter = lvim.builtin.treesitter
+
+  treesitter.auto_install = true
+
+  treesitter.autotag.enable = true
+  treesitter.rainbow.enable = true
+
+  treesitter.textsubjects.enable = true
+end
+
+return M
