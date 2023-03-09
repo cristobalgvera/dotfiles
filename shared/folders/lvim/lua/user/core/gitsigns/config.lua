1a33d80ed27c43540cda8543fe0952b00305db3e
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-03-09 13:15:55 -0300

    fix(lvim): add missing setup function in gitsings config

--- shared/folders/lvim/lua/user/core/gitsigns/config.lua
+++ shared/folders/lvim/lua/user/core/gitsigns/config.lua
@@ -1,8 +1,14 @@
-local gitsigns_opts = lvim.builtin.gitsigns.opts
-local gitsigns_utils = require("user.core.gitsigns.utils")
+local M = {}
 
-gitsigns_opts.current_line_blame = false
-gitsigns_opts.current_line_blame_opts.delay = 300
+M.setup = function()
+  local gitsigns_opts = lvim.builtin.gitsigns.opts
+  local gitsigns_utils = require("user.core.gitsigns.utils")
 
-gitsigns_opts.current_line_blame_formatter_opts.relative_time = true
-gitsigns_opts.current_line_blame_formatter = gitsigns_utils.format_current_line_blame
+  gitsigns_opts.current_line_blame = false
+  gitsigns_opts.current_line_blame_opts.delay = 300
+
+  gitsigns_opts.current_line_blame_formatter_opts.relative_time = true
+  gitsigns_opts.current_line_blame_formatter = gitsigns_utils.format_current_line_blame
+end
+
+return M
