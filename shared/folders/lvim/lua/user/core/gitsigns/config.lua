ca89e99a05ab315ce0688c6bf6e68fce4d9f8af8
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-04-17 11:40:29 -0400

    refactor(lvim): simplify gitsigns blame information format

--- shared/folders/lvim/lua/user/core/gitsigns/config.lua
+++ shared/folders/lvim/lua/user/core/gitsigns/config.lua
@@ -2,13 +2,12 @@ local M = {}
 
 M.setup = function()
   local gitsigns_opts = lvim.builtin.gitsigns.opts
-  local gitsigns_utils = require("user.core.gitsigns.utils")
 
   gitsigns_opts.current_line_blame = false
   gitsigns_opts.current_line_blame_opts.delay = 300
 
-  gitsigns_opts.current_line_blame_formatter_opts.relative_time = true
-  gitsigns_opts.current_line_blame_formatter = gitsigns_utils.format_current_line_blame
+  -- Commit blame example: "ﰖ John Doe, 1 month ago • feat(gitsigns): add blame format"
+  gitsigns_opts.current_line_blame_formatter = "ﰖ <author>, <author_time:%R> • <summary>"
 end
 
 return M
