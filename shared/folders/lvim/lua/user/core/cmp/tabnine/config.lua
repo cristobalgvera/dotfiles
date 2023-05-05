c5ffb6f2e7b37cee72343f665095c2249043c399
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-05 10:17:18 -0400

    fix(lvim): change cmp tabnine configuration setup

--- shared/folders/lvim/lua/user/core/cmp/tabnine/config.lua
+++ shared/folders/lvim/lua/user/core/cmp/tabnine/config.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup = function()
   local tabnine = reload("cmp_tabnine.config")
 
-  tabnine.setup({
+  tabnine:setup({
     max_lines = 1000,
     max_num_results = 3,
     sort = true,
