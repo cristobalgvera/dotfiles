52565ea0685e4ce41c4a0eb5e9ba900bb7898d86
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-24 09:38:30 -0400

    refactor(lvim): set proper markdown-cli installation to format markdowns with null-ls

--- shared/folders/lvim/lua/user/core/lsp/null-ls/linters.lua
+++ shared/folders/lvim/lua/user/core/lsp/null-ls/linters.lua
@@ -4,7 +4,7 @@ M.setup = function()
   local linters = reload("lvim.lsp.null-ls.linters")
 
   linters.setup({
-    { command = "markdownlint" }, -- npm install -g markdownlint
+    { command = "markdownlint" }, -- npm install -g markdownlint-cli
     { command = "shellcheck" }, -- brew install shellcheck
     { command = "eslint_d" }, -- npm install -g eslint_d
   })
