678ce3d41186ae743fb68047b159272debdad63a
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-12-07 11:26:07 -0300

    feat(lvim): increase format timeout

--- shared/folders/lvim/lua/user/lvim/options.lua
+++ shared/folders/lvim/lua/user/lvim/options.lua
@@ -1,8 +1,11 @@
 local M = {}
 
 M.setup = function()
+  local utils = require("user.utils")
+
   lvim.format_on_save.enabled = true
-  lvim.format_on_save.pattern = "*.lua"
+  lvim.format_on_save.timeout = utils.constants.FORMAT_TIMEOUT
+  lvim.format_on_save.pattern = "*"
 end
 
 return M
---
+++ shared/folders/lvim/lua/user/utils/constants.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.FORMAT_TIMEOUT = 3000
+
+return M
