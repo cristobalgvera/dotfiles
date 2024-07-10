a2910b2c2ccb7be78f773434be6d0c8163681035
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-07-10 01:34:33 -0400

    feat(nvim): configure preview window for telescope

--- shared/folders/lvim/lua/user/core/telescope/config.lua
+++ shared/folders/lvim/lua/user/core/telescope/config.lua
@@ -1,6 +1,25 @@
 local M = {}
 
 M.setup = function()
+  local defaults = lvim.builtin.telescope.defaults
+
+  defaults.sorting_strategy = "ascending"
+  defaults.layout_strategy = "flex"
+  defaults.layout_config = {
+    prompt_position = "top",
+    horizontal = {
+      preview_cutoff = 100,
+      preview_width = 0.5,
+    },
+    vertical = {
+      mirror = true,
+      preview_cutoff = 0.4,
+    },
+    flip_columns = 96,
+    height = 0.94,
+    width = 0.86,
+  }
+
   lvim.builtin.telescope.on_config_done = function(telescope)
     local telescope_utils = require("user.core.telescope.utils")
 
