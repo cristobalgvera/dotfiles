ab7a1fb9b2c5644b5a59657928a30ac9ec6a5d54
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-07-10 03:29:42 -0400

    feat(nvim): setup autotag using treesitter

--- shared/folders/lvim/lua/user/core/treesitter/config.lua
+++ shared/folders/lvim/lua/user/core/treesitter/config.lua
@@ -1,13 +1,19 @@
 local M = {}
 
+local function setup_autotag()
+  local autotag = reload("nvim-ts-autotag")
+
+  autotag.setup()
+end
+
 M.setup = function()
   local treesitter = lvim.builtin.treesitter
 
   treesitter.auto_install = true
 
-  treesitter.autotag.enable = true
-
   treesitter.textsubjects.enable = true
+
+  setup_autotag()
 end
 
 return M
