70bd2e87b3dab7e1e5d2194daa0d0a9225b15f23
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-08-19 13:44:15 -0400

    fix(nvim): don't modify package.json for markdown preview when installing it

--- shared/nvim/lua/plugins/markdown-preview.lua
+++ shared/nvim/lua/plugins/markdown-preview.lua
@@ -6,7 +6,7 @@ return {
   build = function(plugin)
     -- HACK: Install using `npx` if available
     if vim.fn.executable "npx" then
-      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx -y yarn install")
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && COREPACK_ENABLE_AUTO_PIN=0 npx -y yarn install --frozen-lockfile")
     else
       vim.cmd [[Lazy load markdown-preview.nvim]]
       vim.fn["mkdp#util#install"]()
