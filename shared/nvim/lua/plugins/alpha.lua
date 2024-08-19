17e17dd4e94e5804a538115a8e18d8eddfa70ef4
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-08-19 18:49:54 -0400

    feat(nvim): improve alpha dashboard

--- shared/nvim/lua/plugins/alpha.lua
+++ shared/nvim/lua/plugins/alpha.lua
@@ -2,6 +2,8 @@
 return {
   "goolord/alpha-nvim",
   opts = function(_, opts)
+    -- Header customization
+    opts.section.header.opts.hl = "Keyword"
     opts.section.header.val = {
       "   ⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣴⣶⣶⣶⣶⣶⠶⣶⣤⣤⣀      ",
       "⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⠁⠀⢀⠈⢿⢀⣀⠀⠹⣿⣿⣿⣦⣄   ",
@@ -16,6 +18,34 @@ return {
       "                   ",
       "          Dani y Cris       ",
       "                   ",
+    }
+
+    -- Button customization
+    local get_icon = require("astroui").get_icon
+
+    opts.section.buttons.val = {
+      opts.button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+      opts.button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+      opts.button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      opts.button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+    }
+
+    for _, button in ipairs(opts.section.buttons.val) do
+      button.opts.hl = "Function"
+      button.opts.hl_shortcut = "Comment"
+    end
+
+    -- Footer customization
+    opts.section.footer.opts.hl = "String"
+
+    -- Layout customization
+    opts.config.layout = {
+      { type = "padding", val = 2 },
+      opts.section.header,
+      { type = "padding", val = 2 },
+      opts.section.buttons,
+      { type = "padding", val = 2 },
+      opts.section.footer,
     }
   end,
 }
