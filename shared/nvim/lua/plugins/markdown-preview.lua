c06c56ea037dcc2c6e1263f90a01c370422835fe
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-01 20:37:22 -0400

    feat(nvim): improve markdown preview configuration

--- shared/nvim/lua/plugins/markdown-preview.lua
+++ shared/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,9 @@
 ---@type LazySpec
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
   build = function(plugin)
-    -- HACK: Install using `npx` if available
+    -- HACK: Install using `npx` (using `yarn`) if available
+    ---@see https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2254280534
     if vim.fn.executable "npx" then
       vim.cmd("!cd " .. plugin.dir .. " && cd app && COREPACK_ENABLE_AUTO_PIN=0 npx -y yarn install --frozen-lockfile")
     else
@@ -12,7 +11,26 @@ return {
       vim.fn["mkdp#util#install"]()
     end
   end,
+  ft = { "markdown", "markdown.mdx" },
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   init = function()
-    if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
+    local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
+    vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
   end,
+  dependencies = {
+    { "AstroNvim/astroui", opts = { icons = { Markdown = "" } } },
+    {
+      "AstroNvim/astrocore",
+      optional = true,
+      opts = function(_, opts)
+        local maps = opts.mappings
+        local prefix = "<Leader>M"
+
+        maps.n[prefix] = { desc = require("astroui").get_icon("Markdown", 1, true) .. "Markdown" }
+        maps.n[prefix .. "p"] = { "<cmd>MarkdownPreview<cr>", desc = "Preview" }
+        maps.n[prefix .. "s"] = { "<cmd>MarkdownPreviewStop<cr>", desc = "Stop preview" }
+        maps.n[prefix .. "t"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview" }
+      end,
+    },
+  },
 }
