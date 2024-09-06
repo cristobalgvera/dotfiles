fc6c3f5ecf09b58aa032af1dfd7ef8cfce31e470
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-06 10:29:34 -0400

    refactor(nvim): remove custom neotest-jest adapter config as it was included in community plugins

--- shared/nvim/lua/plugins/typescript.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "nvim-neotest/neotest",
-  dependencies = { "nvim-neotest/neotest-jest" },
-  opts = function(_, opts)
-    if not opts.adapters then opts.adapters = {} end
-    table.insert(opts.adapters, require "neotest-jest" {})
-  end,
-}
