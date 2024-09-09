228f4356289fc6406beb5b5c0cbb07e7a060e478
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-09 15:44:22 -0300

    feat(nvim): add `render-markdown.nvim` community integration

--- shared/nvim/lua/community.lua
+++ shared/nvim/lua/community.lua
@@ -17,6 +17,7 @@ return {
   utils.add_git "blame-nvim",
   utils.add_indent "mini-indentscope",
   utils.add_markdown_and_latex "markdown-preview-nvim",
+  utils.add_markdown_and_latex "render-markdown-nvim",
   utils.add_media "vim-wakatime",
   utils.add_motion "flash-nvim",
   utils.add_motion "nvim-surround",
