f76bc9a57b7d19077520ed551dac8f42bbcace13
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-10 12:02:40 -0300

    feat(nvim): add metadata related with the creation and update of notes

--- shared/nvim/lua/plugins/obsidian.lua
+++ shared/nvim/lua/plugins/obsidian.lua
@@ -77,17 +77,33 @@ return {
       note_frontmatter_func = function(note)
         if note.title then note:add_alias(note.title) end
 
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+        local default_metadata = {
+          id = note.id,
+          aliases = note.aliases,
+          tags = note.tags,
+        }
 
         -- `note.metadata` contains any manually added fields in the frontmatter.
         -- So here we just make sure those fields are kept in the frontmatter.
         if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
           for k, v in pairs(note.metadata) do
-            out[k] = v
+            default_metadata[k] = v
           end
         end
 
-        return out
+        if not default_metadata.created_at then
+          default_metadata.created_at = {
+            date = vim.fn.strftime "%Y-%m-%d",
+            time = vim.fn.strftime "%H:%M",
+          }
+        end
+
+        default_metadata.updated_at = {
+          date = vim.fn.strftime "%Y-%m-%d",
+          time = vim.fn.strftime "%H:%M",
+        }
+
+        return default_metadata
       end,
 
       attachments = {
