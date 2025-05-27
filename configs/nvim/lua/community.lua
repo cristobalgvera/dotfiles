c5f0433eb2bee5d70c9ac2ff8cd1a2ac3361df06
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-05-27 12:35:27 -0400

    feat(nvim): add `python` support

--- configs/nvim/lua/community.lua
+++ configs/nvim/lua/community.lua
@@ -34,6 +34,7 @@ return {
   utils.add_pack "lua",
   utils.add_pack "markdown",
   utils.add_pack "proto",
+  utils.add_pack "python",
   utils.add_pack "rust",
   utils.add_pack "tailwindcss",
   utils.add_pack "terraform",
