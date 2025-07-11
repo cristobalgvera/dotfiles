591b6953473318e55993952c6816167c18eebde0
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-07-10 20:52:35 -0400

    feat(nvim): replace `avante.nvim` with `codecompanion.nvim`

--- configs/nvim/lua/plugins/avante.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  "yetone/avante.nvim",
-  opts = function(_, opts)
-    if not opts.behavior then opts.behavior = {} end
-    opts.behavior.enable_cursor_planning_mode = true
-  end,
-}
