b670955d5fff7cdeeffdbeb8594fcdab10a74361
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-08-06 16:29:33 -0400

    feat(nvim): skip noisy notifications from being shown

--- configs/nvim/lua/plugins/noice.lua
+++ configs/nvim/lua/plugins/noice.lua
@@ -2,7 +2,40 @@
 return {
   "folke/noice.nvim",
   opts = function(_, opts)
-    if not opts.presets then opts.presets = {} end
+    local utils = require "astrocore"
+
+    local unwanted_notifications = {
+      -- INFO: This is a workaround for the "No information available" message
+      -- that appears when using the `:LspInfo` command and one of the LSPs
+      -- does not provide any information.
+      {
+        event = "notify",
+        message = "No information available",
+      },
+      -- INFO: Do not show deprecation messages for library functions
+      -- that use `vim.tbl_islist` internally.
+      {
+        event = "msg_show",
+        message = "vim.tbl_islist is deprecated",
+      },
+    }
+
+    opts.routes = opts.routes or {}
+    for _, notification in ipairs(unwanted_notifications) do
+      utils.list_insert_unique(opts.routes, {
+        {
+          filter = {
+            event = notification.event,
+            find = notification.message,
+          },
+          opts = {
+            skip = true,
+          },
+        },
+      })
+    end
+
+    opts.presets = opts.presets or {}
     opts.presets.lsp_doc_border = true
   end,
 }
