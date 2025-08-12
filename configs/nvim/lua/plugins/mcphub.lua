d2a931e21a4173141de0e7f166da4ed7754a05a1
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-08-11 23:53:17 -0400

    feat(nvim): add install `mcp-hub` when building the plugin

--- configs/nvim/lua/plugins/mcphub.lua
+++ configs/nvim/lua/plugins/mcphub.lua
@@ -1,8 +1,6 @@
 ---@type LazySpec
 return {
   "ravitemer/mcphub.nvim",
-  opts = function(_, opts)
-    opts.port = 37373
-    opts.config = vim.fn.expand "~/.config/mcphub/servers.json"
-  end,
+  build = "npm install -g mcp-hub@latest",
+  opts = {},
 }
