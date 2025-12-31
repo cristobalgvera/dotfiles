db666c027ad79e032f4812535960c9b88dbabf06
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2025-12-31 18:41:59 -0300

    feat(nvim): remove unwanted notification silence since it is not longer required

--- configs/nvim/lua/plugins/noice.lua
+++ configs/nvim/lua/plugins/noice.lua
@@ -4,46 +4,6 @@ return {
   opts = function(_, opts)
     local utils = require "astrocore"
 
-    local unwanted_notifications = {
-      -- INFO: This is a workaround for the "No information available" message
-      -- that appears when using the `:LspInfo` command and one of the LSPs
-      -- does not provide any information.
-      {
-        event = "notify",
-        message = "No information available",
-      },
-      -- INFO: Do not show deprecation messages for library functions
-      -- that use `vim.tbl_islist` internally.
-      {
-        event = "msg_show",
-        kind = "echomsg",
-        message = "vim.tbl_islist is deprecated",
-      },
-      -- INFO: Do not show error messages related to `vim.schedule` callbacks.
-      -- These errors are often non-critical and can be ignored.
-      {
-        event = "msg_show",
-        kind = "lua_error",
-        message = "Error executing vim.schedule lua callback",
-      },
-    }
-
-    opts.routes = opts.routes or {}
-    for _, notification in ipairs(unwanted_notifications) do
-      utils.list_insert_unique(opts.routes, {
-        {
-          filter = {
-            event = notification.event,
-            kind = notification.kind,
-            find = notification.message,
-          },
-          opts = {
-            skip = true,
-          },
-        },
-      })
-    end
-
     opts.presets = opts.presets or {}
     opts.presets.lsp_doc_border = true
   end,
